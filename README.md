0393432d341b9746a39bb4d589ba9fbdbc5f7b8b
Juan Sebastian Cadena <10219+sebastiancadena@users.noreply.github.com>
2023-06-29 13:11:57 -0500

    Fwirst release!

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# Welcome to Streamlit!
+# s3-encrypted-file-donwloader
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+Simple utility to decrypt encrypted zip files
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,54 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+import pyzipper
+import requests
 import streamlit as st
+import io
+import os
+import tempfile
+import zipfile
 
-"""
-# Welcome to Streamlit!
+def get_data(url, password):
+    # Download the zip to a temporary location
+    r = requests.get(url)
+    # Save the zip content to a temporary file
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_file.write(r.content)
+        temp_file_path = temp_file.name
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+    # Decrypt the zip and generate a new zip file without a password
+    with pyzipper.AESZipFile(temp_file_path) as zf:
+        zf.pwd = password.encode()
+        # Extract the decrypted zip file to a temporary directory
+        with tempfile.TemporaryDirectory() as temp_dir:
+            zf.extractall(temp_dir)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+            # Compress the extracted files into a new zip file
+            with io.BytesIO() as compressed_zip:
+                with zipfile.ZipFile(compressed_zip, 'w', compression=zipfile.ZIP_DEFLATED) as new_zip:
+                    for root, _, files in os.walk(temp_dir):
+                        for file in files:
+                            file_path = os.path.join(root, file)
+                            new_zip.write(file_path, arcname=os.path.relpath(file_path, temp_dir))
+                data = compressed_zip.getvalue()
+    # Delete the temporary file
+    if temp_file_path:
+        os.remove(temp_file_path)
+    return data
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+st.set_page_config(layout="wide", page_title='Utilidad para desencriptar archivos cifrados')
+params = st.experimental_get_query_params()
 
+with st.form(key='download_form'):
+    url = st.text_input(label='Enlace de descarga del archivo cifrado', value=params.get('s3_url', [''])[0])
+    password = st.text_input(label='Contraseña', type='password', value=params.get('password', [''])[0])
+    submitted = st.form_submit_button(label='Desencriptar')
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    if submitted:
+        if 'raw_zip' in st.session_state:
+            del st.session_state.raw_zip
+        st.session_state.raw_zip = get_data(url, password)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+if 'raw_zip' in st.session_state:
+    st.download_button(label="Descargar", data=st.session_state.raw_zip,
+                        file_name='test_results.zip',
+                        mime='application/zip')
 
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
